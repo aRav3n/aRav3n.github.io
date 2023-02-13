234a84ac3aa2223719fd2b3d5d89855e1903fee3
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-02-13 21:06:57 +0530

    Create function to automatically populate nav list
    Function creats <li> and <a> items from name and link

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // Initiate global variables
 const navButton = document.querySelector('#navIcon');
-let navItem = {};
-let navItems = [];
+let navList = [];
 
 // Populate list of navItems
-function 
+navList.push(addNavItem('4FunnerVibes', '/index.html'));
+navList.push(addNavItem('About Us', '/us.html'));
+navList.push(addNavItem('About Our Rig', '/rig.html'));
+navList.push(addNavItem('Travel Highlights', '/highlights.html'));
 
-// Create menu list with links and (text || icon)
+// Create menu list with links
 
 // Display menu on click
+
+
+function addNavItem(name, url) {
+    let string = '<li><a href="';
+    string += url;
+    string += '" target="_blank" rel="noreferrer noopener">';
+    string += name;
+    string += '</a></li>';
+    return string;
+};
