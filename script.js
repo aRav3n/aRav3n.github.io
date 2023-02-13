923d8017efe426f5736a20e8b73837f8d6a5a964
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-02-13 14:37:27 +0530

    Begin JS code for the pop up navbar
    Also added social media links in separate list as I feel it is a
    better look

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initiate global variables
+const navButton = document.querySelector('#navIcon');
+let navItem = {};
+let navItems = [];
+
+// Populate list of navItems
+function 
+
+// Create menu list with links and (text || icon)
+
+// Display menu on click
