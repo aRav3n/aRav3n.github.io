1ce5207bd4ec8ba85000bad94b71902ea99fceeb
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-02-14 22:26:54 +0530

    Enable normal horizontal menu if page is wide enough

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 // Initiate global variables
 const navbarPopUp = document.querySelector('#navbarPopUp');
 const navButton = document.querySelector('#navIcon');
-let navList4fv = create4fvNavList();
+const navMenuHorizontal = document.querySelector('#navMenuHorizontal')
 let windowWidth = window.innerWidth;
 
-// Recalculate width upon window resize
 window.addEventListener('resize',() => {
     windowWidth = window.innerWidth;
+    selectCorrectNavMenu();
 });
 
-// Display menu on click for narrower screens
-navButton.addEventListener('click',() => {
-    if(!navbarPopUp.classList.contains('linksDisplayed')) {
-        navbarPopUp.appendChild(create4fvNavList('navListPopUp'));
-        navbarPopUp.classList.add('linksDisplayed');
-        navButton.classList.add('navIconActive');
-    } else {
-        navbarPopUp.innerHTML = '';
-        navbarPopUp.classList.remove('linksDisplayed');
-        navButton.classList.remove('navIconActive');
-    }
+window.addEventListener('DOMContentLoaded',() => {
+    selectCorrectNavMenu();
 });
+
+function selectCorrectNavMenu() {
+    if(windowWidth < 650) {
+        enablePopUpNav();
+    } else {
+        displayHorizontalNav();
+    };
+};
+
+function addNavItem(name, url) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.classList.add('navLink');
+    a.setAttribute('href', url);
+    a.innerText = name;
+    li.appendChild(a);
+    return li;
+};
 
 function create4fvNavList(classToAdd) {
     let ul = document.createElement('ul');
@@ -32,12 +41,31 @@ function create4fvNavList(classToAdd) {
     return ul;
 };
 
-function addNavItem(name, url) {
-    let li = document.createElement('li');
-    let a = document.createElement('a');
-    a.classList.add('navLink');
-    a.setAttribute('href', url);
-    a.innerText = name;
-    li.appendChild(a);
-    return li;
+function displayHorizontalNav() {
+    navButton.style.display = 'none';
+    if(!navMenuHorizontal.classList.contains('linksDisplayed')) {
+        console.log('adding linksDisplayed class and making menu');
+        navMenuHorizontal.classList.add('linksDisplayed')
+        navMenuHorizontal.style.display = 'flex';
+        navMenuHorizontal.appendChild(create4fvNavList('navListDisplay'));
+    };
+};
+
+function enablePopUpNav() {
+    navMenuHorizontal.style.display = 'none';
+    navMenuHorizontal.classList.remove('linksDisplayed');
+    navMenuHorizontal.innerHTML = '';
+    console.log('removed class from horizontal menu');
+    navButton.style.display = 'block';
+    navButton.addEventListener('click',() => {
+        if(!navbarPopUp.classList.contains('linksDisplayed')) {
+            navbarPopUp.appendChild(create4fvNavList('navListPopUp'));
+            navbarPopUp.classList.add('linksDisplayed');
+            navButton.classList.add('navIconActive');
+        } else {
+            navbarPopUp.innerHTML = '';
+            navbarPopUp.classList.remove('linksDisplayed');
+            navButton.classList.remove('navIconActive');
+        }
+    });
 };
