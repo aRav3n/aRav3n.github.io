1a51ff2e391ae22df635cd10ffb7bfa92a7fdd61
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-04-16 17:01:27 -0600

    build: add all content to footer
    begin styling footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./style.css";
+
+const buildMainHtml = () => {
+  /*
+    <header>
+      <div id="headerPhoto">
+        <img
+          src="../src/media/main-photo-300w.jpg"
+          alt="Andy in a tropical setting"
+          srcset="
+            ../src/media/main-photo-300w.jpg   300w,
+            ../src/media/main-photo-300w.jpg   400w,
+            ../src/media/main-photo-600w.jpg   600w,
+            ../src/media/main-photo-800w.jpg   800w,
+            ../src/media/main-photo-1000w.jpg 1000w,
+            ../src/media/main-photo-2000w.jpg 2000w
+          "
+          sizes="(max-width: 350px)"
+        />
+        <h1></h1>
+      </div>
+    </header>
+  */
+}
