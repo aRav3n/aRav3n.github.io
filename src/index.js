b547bc22707771640a265c0d4635b84e17461583
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-04-29 16:19:57 -0600

    build: complete function to build project grid
    builds the grid based on an array so should be simpler to add new
    projects to the portfolio in the future

--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
 import "./style.css";
 import GithubIcon from "../src/media/icons8-github.svg";
 import OpenLinkIcon from "../src/media/icons8-open.svg";
+import battleshipImg from "../src/media/Screenshots/battleship.jpeg";
+import businessPageImg from "../src/media/Screenshots/business_page.jpeg";
+import calculatorImg from "../src/media/Screenshots/calculator.jpeg";
+import dashboardImg from "../src/media/Screenshots/dashboard.jpeg";
+import etchASketchImg from "../src/media/Screenshots/etch-a-sketch.jpeg";
+import libraryImg from "../src/media/Screenshots/library.jpeg";
+import rockPaperScissorsImg from "../src/media/Screenshots/rock_paper_scissors.jpeg";
+import signupImg from "../src/media/Screenshots/signup.jpeg";
+import tickTackToeImg from "../src/media/Screenshots/tic-tac-toe.jpeg";
+import toDoListImg from "../src/media/Screenshots/to-do-list.jpeg";
 
-const projectList = [];
+const buildProjectList = () => {
+  const projectList = [];
 
-const addProject = (
-  screenshotSrc,
-  imageAltText,
-  projectName,
-  projectDescription
-) => {
-  const projectObject = {};
-  projectObject.screenshot = screenshotSrc;
-  projectObject.name = projectName;
-  projectObject.description = projectDescription;
-  projectObject.alt = imageAltText;
-  projectList.push(projectObject);
+  const addProject = (
+    screenshotSrc,
+    imageAltText,
+    projectName,
+    projectDescription,
+    gitHubLink,
+    livePreviewLink
+  ) => {
+    const projectObject = {};
+    projectObject.screenshot = screenshotSrc;
+    projectObject.name = projectName;
+    projectObject.description = projectDescription;
+    projectObject.alt = imageAltText;
+    projectObject.git = gitHubLink;
+    projectObject.preview = livePreviewLink;
+    projectList.push(projectObject);
+  };
+
+  return projectList;
 };
 
 const buildProjectDOM = () => {
+  const projectList = buildProjectList();
   const mainDomElement = document.querySelector("main");
 
   const heading = document.createElement("h2");
-  heading.innerHTML = "My Work";
+  heading.innerHTML = "Examples of My Work";
 
   const projectGrid = document.createElement("div");
   projectGrid.classList.add("projectGrid");
@@ -32,18 +51,47 @@ const buildProjectDOM = () => {
     const projectDiv = document.createElement("div");
     projectDiv.classList.add("project");
 
-    const screenshot = document.createElement("img");
+    const screenshot = new Image();
     screenshot.setAttribute("alt", projectObject.alt);
-    screenshot.setAttribute("src", projectObject.screenshot);
+    screenshot.src = projectObject.screenshot;
     const screenshotDiv = document.createElement("div");
     screenshotDiv.appendChild(screenshot);
+
+    const titleDiv = document.createElement("div");
+    const projectTitle = document.createElement("h3");
+    projectTitle.innerHTML = projectObject.name;
+    const linkDiv = document.createElement;
+    const gitLink = document.createElement("a");
+    gitLink.setAttribute("href", projectObject.git);
+    const gitImg = new Image();
+    gitImg.src = GithubIcon;
+    gitImg.setAttribute("alt", "project git hub");
+    gitLink.appendChild(gitImg);
+    const previewLink = document.createElement("a");
+    previewLink.setAttribute("href", projectObject.preview);
+    const previewImg = new Image();
+    previewImg.src = OpenLinkIcon;
+    previewImg.setAttribute("alt", "live preview of the project");
+    previewLink.appendChild(previewImg);
+    linkDiv.appendChild(gitLink);
+    linkDiv.appendChild(previewLink);
+    titleDiv.appendChild(projectTitle);
+    titleDiv.appendChild(linkDiv);
+
+    const descriptionP = document.createElement("p");
+    descriptionP.innerHTML = projectObject.description;
+    const detailDiv = document.createElement("div");
+    detailDiv.appendChild(titleDiv);
+    detailDiv.appendChild(descriptionP);
+
     projectDiv.appendChild(screenshotDiv);
-
-    const linkDiv = document.createElement
-
+    projectDiv.appendChild(detailDiv);
+    projectGrid.appendChild(projectDiv);
   }
 
   mainDomElement.innerHTML = "";
   mainDomElement.appendChild(heading);
   mainDomElement.appendChild(projectGrid);
 };
+
+buildProjectDOM();
